7fc70e382c44f3b230dde9da89d9a95350455785
Kagiso Malepe <kagiso@cagegroup.co.za>
2021-03-23 14:15:54 +0200

    Log modem status variables

--- retrieveModemStatus.js
+++ retrieveModemStatus.js
@@ -9,6 +9,27 @@
       }
   };
 
+  function writeToFile(fileName, path, data, append){
+    fs = require('fs');
+    if (append) {
+      fs.appendFile(path+fileName, data, function (err) {
+      if (err) throw err;
+        console.log('Updated!');
+      });
+    }else {
+      fs.writeFile(path+fileName, data, function (err) {
+        if (err) throw err;
+        console.log('Replaced!');
+      });
+    }
+  }
+
+  function genFileName(){
+    let dateObj = new Date();
+    let fileName = ('0'+String(dateObj.getUTCMonth()+1)).slice(-2)+String(dateObj.getUTCFullYear());
+    return fileName+'.csv';
+  }
+
   function callback(error, response, body) {
       if (!error && response.statusCode == 200) {
         const cheerio = require('cheerio');
@@ -17,7 +38,7 @@
         // Eb/No (dB)
         // Modem Temperature (deg C)
         // BUC Temperature (deg C)
-        let CircuitID = $('input[name="C8_15"]')[0].attribs.value;
+        let CircuitID = $('input[name="C8_15"]')[0].attribs.value.trim();
         let SerialNumber = $('input[name="MR3_20"]')[0].attribs.value;
         let SoftwareRevision = $('input[name="MR3_3"]')[0].attribs.value;
         let RemoteLocal = $('input[name="MR3_25"]')[0].attribs.value;
@@ -39,14 +60,19 @@
         let Redundancy = $('input[name="MR3_17"]')[0].attribs.value;
         let RxSignalLevel = $('input[name="MR3_99"]')[0].attribs.value;
         let LNBCurrent = $('input[name="B1_6"]')[0].attribs.value;
+        let now = new Date();
+        let dateTime = now.toISOString();
+        let dataStream = dateTime+','+CircuitID+','+SerialNumber+','+SoftwareRevision+','+RemoteLocal+','+EventsLogUnreadLines+','+StatisticsLogUnreadLines+','+EbNoRemoteDemodulator+','+TxPowerLevelIncrease+','+TxCarrier+','+ModemTemperature+','+BUCTemperature+','+UnitStatus+','+RxStatus+','+TxStatus+','+ODUStatus+','+LNBStatus+','+EbNo+','+FreqOffset+','+BER+','+Redundancy+','+RxSignalLevel+','+LNBCurrent+'\n';
 
-
-
-       console.log(RxSignalLevel);
+        writeToFile(genFileName(),'./',dataStream,'1');
+       // console.log(RxSignalLevel+"\n");
+       // console.log(CircuitID+"\n");
       }
   }
 
 request(options, callback);
+
+
 
 
 
